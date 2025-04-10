e4a114ccc0952831e4405467b94a68e947423da6
Flinta Ovidiu <ovidiu.flinta@gmail.com>
2025-04-10 20:59:06 +0300

    Add lab3

--- 02_Inventory/src/main/java/inventory/model/Product.java
+++ 02_Inventory/src/main/java/inventory/model/Product.java
@@ -1,11 +1,14 @@
 
 package inventory.model;
 
-
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.Objects;
+
+
 public class Product {
-    
+
     // Declare fields
     private ObservableList<Part> associatedParts;// = FXCollections.observableArrayList();
     private int productId;
@@ -25,7 +28,20 @@ public class Product {
         this.max = max;
         this.associatedParts= partList;
     }
-    
+
+    public Product(int productId, String name, double price, int inStock, int min, int max){
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.inStock = inStock;
+        this.min = min;
+        this.max = max;
+    }
+
+    public Product() {
+
+    }
+
     // Getters
     public ObservableList<Part> getAssociatedParts() {
         return associatedParts;
@@ -54,10 +70,10 @@ public class Product {
     public int getMax() {
         return max;
     }
-    
+
     // Setters
     public void setAssociatedParts(ObservableList<Part> associatedParts) {
-        this.associatedParts = associatedParts;
+        associatedParts = associatedParts;
     }
 
     public void setProductId(int productId) {
@@ -83,72 +99,39 @@ public class Product {
     public void setMax(int max) {
         this.max = max;
     }
-    
+
     // Other methods
     public void addAssociatedPart(Part part) {
         associatedParts.add(part);
     }
-    
+
     public void removeAssociatedPart(Part part) {
         associatedParts.remove(part);
     }
-    
+
     public Part lookupAssociatedPart(String searchItem) {
         for(Part p:associatedParts) {
-            if(p.getName().contains(searchItem) || new Integer(p.getPartId()).toString().equals(searchItem)) return p;
+            if(p.getName().contains(searchItem)) return p;
         }
         return null;
-    }
-    
-    /**
-     * Generate an error message for invalid values in a product
-     * and evaluate whether the sum of the price of associated parts
-     * is less than the price of the resulting product.
-     * A valid product will return an empty error message string.
-     * @param name
-     * @param min
-     * @param max
-     * @param inStock
-     * @param price
-     * @param parts
-     * @param errorMessage
-     * @return 
-     */
-    public static String isValidProduct(String name, double price, int inStock, int min, int max, ObservableList<Part> parts, String errorMessage) {
-        double sumOfParts = 0.00;
-        for (int i = 0; i < parts.size(); i++) {
-            sumOfParts += parts.get(i).getPrice();
-        }
-        if (name.equals("")) {
-            errorMessage += "A name has not been entered. ";
-        }
-        if (min < 0) {
-            errorMessage += "The inventory level must be greater than 0. ";
-        }
-        if (price < 0.01) {
-            errorMessage += "The price must be greater than $0. ";
-        }
-        if (min > max) {
-            errorMessage += "The Min value must be less than the Max value. ";
-        }
-        if(inStock < min) {
-            errorMessage += "Inventory level is lower than minimum value. ";
-        }
-        if(inStock > max) {
-            errorMessage += "Inventory level is higher than the maximum value. ";
-        }
-        if (parts.size() < 1) {
-            errorMessage += "Product must contain at least 1 part. ";
-        }
-        if (sumOfParts > price) {
-            errorMessage += "Product price must be greater than cost of parts. ";
-        }
-        return errorMessage;
     }
 
     @Override
     public String toString() {
         return "P,"+this.productId+","+this.name+","+this.price+","+this.inStock+","+
                 this.min+","+this.max;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && Double.compare(product.price, price) == 0 && inStock == product.inStock && min == product.min && max == product.max && Objects.equals(associatedParts, product.associatedParts) && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(associatedParts, productId, name, price, inStock, min, max);
     }
 }
